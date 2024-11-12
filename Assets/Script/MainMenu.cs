ceaf07d86f0274149d6354b27f6679c30477335f
Muhammad Umar <umer.ramzan375@gmail.com>
2024-11-12 16:56:26 +0500

    Admob Ads Integrated
    Admob Ads Integrated

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Video;
 using System.Collections;
+using AL.Monetization.CustomAdmobAds.Scripts;
 
 public class MainMenu : MonoBehaviour
 {
@@ -24,5 +25,9 @@ public class MainMenu : MonoBehaviour
     {
         // Load Level_8 scene
         SceneManager.LoadScene("Level_1");
+    }
+    public void ShowInterstitial()
+    {
+        AdManager.instance.ShowInterstitial(); // paste this line anywhere you want to show interstitial 
     }
 }
